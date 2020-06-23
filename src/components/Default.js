4988d4b2f0bb710110c4fb2848467842c9276d18
mentorxp <mentor.xp@gmail.com>
2020-06-24 00:47:35 +0100

    Component Folder updated

---
+++ src/components/Default.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Default extends Component {
+    render() {
+        return(
+        
+            <div>
+                <h3>Pagina ----  DEFAULT</h3>
+            </div>
+            
+        )
+    }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Navbar extends Component {
+    render() {
+        return(
+        
+            <div>
+                <h3>aici ----- Navbar</h3>
+            </div>
+            
+        )
+    }
+}
